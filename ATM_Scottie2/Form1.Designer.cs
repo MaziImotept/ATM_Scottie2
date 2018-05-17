d0975c5ec4ce76b3d0fd764db19bb9da1daf9fac
SCOTTIE STERLING <SCOSTERL@uat.edu>
2018-05-17 03:32:08 -0700

    Add project files.

---
+++ ATM_Scottie2/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace ATM_Scottie2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainmenu = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.login_button = new System.Windows.Forms.Button();
+            this.enterbox = new System.Windows.Forms.TextBox();
+            this.secondarymenu = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.balanceLabel = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.logoutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.mainmenu.SuspendLayout();
+            this.secondarymenu.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainmenu
+            // 
+            this.mainmenu.Controls.Add(this.enterbox);
+            this.mainmenu.Controls.Add(this.login_button);
+            this.mainmenu.Controls.Add(this.label1);
+            this.mainmenu.Location = new System.Drawing.Point(12, 27);
+            this.mainmenu.Name = "mainmenu";
+            this.mainmenu.Size = new System.Drawing.Size(283, 183);
+            this.mainmenu.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(102, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ACCOUNT NAME/NUMBER";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // login_button
+            // 
+            this.login_button.Location = new System.Drawing.Point(105, 126);
+            this.login_button.Name = "login_button";
+            this.login_button.Size = new System.Drawing.Size(75, 23);
+            this.login_button.TabIndex = 1;
+            this.login_button.Text = "LOGIN";
+            this.login_button.UseVisualStyleBackColor = true;
+            this.login_button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // enterbox
+            // 
+            this.enterbox.Location = new System.Drawing.Point(96, 58);
+            this.enterbox.Name = "enterbox";
+            this.enterbox.Size = new System.Drawing.Size(100, 20);
+            this.enterbox.TabIndex = 2;
+            // 
+            // secondarymenu
+            // 
+            this.secondarymenu.Controls.Add(this.textBox1);
+            this.secondarymenu.Controls.Add(this.listBox1);
+            this.secondarymenu.Controls.Add(this.balanceLabel);
+            this.secondarymenu.Controls.Add(this.button4);
+            this.secondarymenu.Controls.Add(this.button3);
+            this.secondarymenu.Controls.Add(this.button2);
+            this.secondarymenu.Controls.Add(this.button1);
+            this.secondarymenu.Controls.Add(this.menuStrip1);
+            this.secondarymenu.Location = new System.Drawing.Point(157, -1);
+            this.secondarymenu.Name = "secondarymenu";
+            this.secondarymenu.Size = new System.Drawing.Size(485, 392);
+            this.secondarymenu.TabIndex = 1;
+            this.secondarymenu.Visible = false;
+            this.secondarymenu.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(94, 129);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 28);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "withdrawl";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(293, 129);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 28);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "deposit";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(128, 292);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(247, 28);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "current balance";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(62, 234);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(134, 28);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "transactionlist";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // balanceLabel
+            // 
+            this.balanceLabel.AutoSize = true;
+            this.balanceLabel.Location = new System.Drawing.Point(225, 339);
+            this.balanceLabel.Name = "balanceLabel";
+            this.balanceLabel.Size = new System.Drawing.Size(25, 13);
+            this.balanceLabel.TabIndex = 5;
+            this.balanceLabel.Text = "100";
+            this.balanceLabel.Visible = false;
+            this.balanceLabel.Click += new System.EventHandler(this.balanceLabel_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(271, 180);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(180, 82);
+            this.listBox1.TabIndex = 2;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.logoutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(485, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // logoutToolStripMenuItem
+            // 
+            this.logoutToolStripMenuItem.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.logoutToolStripMenuItem.Name = "logoutToolStripMenuItem";
+            this.logoutToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.logoutToolStripMenuItem.Text = "logout";
+            this.logoutToolStripMenuItem.Click += new System.EventHandler(this.logoutToolStripMenuItem_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(186, 53);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(96, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(697, 438);
+            this.Controls.Add(this.secondarymenu);
+            this.Controls.Add(this.mainmenu);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.mainmenu.ResumeLayout(false);
+            this.mainmenu.PerformLayout();
+            this.secondarymenu.ResumeLayout(false);
+            this.secondarymenu.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel mainmenu;
+        private System.Windows.Forms.TextBox enterbox;
+        private System.Windows.Forms.Button login_button;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel secondarymenu;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label balanceLabel;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem logoutToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ ATM_Scottie2/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ATM_Scottie2
+{
+    public partial class Form1 : Form
+    {
+        private class Account
+        {
+            private string accountName;
+            private double accountBalance;
+            private string accountNumber;
+
+            public Account(string N, double B, string num)
+            {
+                accountName = N;
+                accountBalance = B;
+                accountNumber = num;
+            }
+
+            public void setAccountname(string name)
+            {
+                accountName = name;
+            }
+            public void setAccountbalance(double balance)
+            {
+                accountBalance = balance;
+            }
+            public void setAccountnumber(string number)
+            {
+                accountNumber = number;
+            }
+
+            public string getAccountname()
+            {
+                return accountName;
+            }
+            public double getAccountbalance()
+            {
+                return accountBalance;
+            }
+            public string getAccountnumber()
+            {
+                return accountNumber;
+            }
+
+
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name = enterbox.Text;
+            Account[] accounts = { };
+            if (Array.IndexOf(accounts, enterbox.Text) != 0)
+            {
+                int I = 0;
+                I++;
+                string number = I.ToString();
+                
+                Account first = new Account(name, 100.00, number);
+                MessageBox
+            }
+
+            mainmenu.Visible = false;
+            secondarymenu.Visible = true;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void balanceLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void logoutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            mainmenu.Visible = true;
+            secondarymenu.Visible = false;
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
